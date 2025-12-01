145cb6647091854e68305d06a923995b6537b908
thanhhien9 <38148773+thanhhien9@users.noreply.github.com>
2025-12-01 17:08:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, send_from_directory, Response
+from flask import Flask, send_from_directory
 import subprocess
 import os
+import sys
 
 app = Flask(__name__, static_folder='public')
 
@@ -9,6 +10,8 @@ def index():
     return send_from_directory('.', 'index.html')
 
 @app.route('/run/<task>', methods=['POST'])
+
+
 def run_task(task):
     mapping = {
         "NAPAS_IBFT": "NAPAS_IBFT.py",
@@ -17,12 +20,21 @@ def run_task(task):
     if task not in mapping:
         return "Task không tồn tại", 400
     # task_file = os.path.join(os.getcwdb, mapping[task])
-    task_file =  os.path.join(os.path.join(os.getcwd(), "lastest_code"), mapping[task])
+    if getattr(sys, 'frozen', False):
+        LOCAL_DIR = os.path.dirname(sys.executable)
+    else:
+        LOCAL_DIR = os.path.dirname(os.path.abspath(__file__))
+    LOCAL_CODE_DIR = os.path.join(LOCAL_DIR, "lastest_code")
+    task_file = os.path.join(LOCAL_CODE_DIR, mapping[task])
+
     try:
         #Gọi file python bằng subprocess
-        result = subprocess.check_output(["python", task_file], text=True, encoding="utf-8", errors="replace")
+        # result = subprocess.check_output(["python", task_file], text=True, encoding="utf-8", errors="replace")
+        result = subprocess.check_output([sys.executable, task_file], text=True, encoding="utf-8", errors="replace")
+
         return f"Kết quả {task}:\n{result}"
     except Exception as e:
         return 'Error: ' + str(e), 500
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # app.run(port=5000, debug=True)
+    app.run(port=5000)
